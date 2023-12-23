49bb71a2f23139539c8668e52587615d61d184aa
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2023-12-23 19:32:30 -0400

    ajustes em animations

--- app/src/components/infoProjetos/info_projeto.tsx
+++ app/src/components/infoProjetos/info_projeto.tsx
@@ -21,44 +21,69 @@ export default function InfoProjetos() {
       </button>
 
       <div
+        id="modal"
         className={` ${
-          modalFechado ? " hidden opacity-0 h-0 w-0" : " opacity-95 "
-        } modal absolute bg-white border-2 border-dark w-64 h-96 white top-0 right-10 z-40 rounded-3xl m-2 shadow-sm shadow-dark`}
+          modalFechado ? " opacity-0 h-0 w-0" : " opacity-95 w-64 h-96 "
+        } modal absolute bg-white border-2 border-dark  white top-0 right-10 z-40 rounded-3xl m-2 shadow-sm shadow-dark`}
       >
-        <h3 className="text-dark text-center text-2xl p-2">Área Insted</h3>
-        <nav
-          className={`
-         ${modalFechado ? "hidden opacity-0" : "opacity-1 "} nav`}
+        <div
+          className="nav
+        transition-opacity duration-150 linear delay-150"
         >
-          <ul>
-            <li>
-              <h4 className="text-xl p-1">Framework</h4>
-              <p className="px-3">Angular 15.2</p>
-            </li>
-            <li>
-              <h4 className="text-xl p-1">Bibliotecas</h4>
-              <a
-                className=""
-                href="https://v15.material.angular.io/"
-                target="blank"
-              >
-                <p className="px-3">Angular Material</p>
-              </a>
-              <a href="https://swiperjs.com/swiper-api" target="blank">
-                <p className="px-3">SwiperJS</p>
-              </a>
-              <a href="https://www.npmjs.com/package/crypto-js" target="blank">
-                <p className="px-3">Crypto-Js</p>
-              </a>
-              <a href="https://www.npmjs.com/package/jwt-decode" target="blank">
-                <p className="px-3">JWT-Decode</p>
-              </a>
-              <a href="https://ngrx.io/guide/store" target="blank">
-                <p className="px-3">Ngrx-Store</p>
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <div className="">
+            <h3
+              className={`'
+         ${modalFechado ? " opacity-0" : "opacity-1 "}
+         text-dark text-center text-2xl p-2 
+         transition-opacity duration-150 linear delay-300
+         '
+         `}
+            >
+              Área Insted
+            </h3>
+          </div>
+          <nav
+            className={`'
+         ${modalFechado ? " opacity-0" : "opacity-1 "} nav  
+         transition-opacity duration-150 linear delay-500
+         '`}
+          >
+            <ul className={`' ${modalFechado ? " opacity-0" : "opacity-1"} ' `}>
+              <li>
+                <h4 className="text-xl p-1">Framework</h4>
+                <p className="px-3">Angular 15.2</p>
+              </li>
+              <li>
+                <h4 className="text-xl p-1">Bibliotecas</h4>
+                <a
+                  className=""
+                  href="https://v15.material.angular.io/"
+                  target="blank"
+                >
+                  <p className="px-3">Angular Material</p>
+                </a>
+                <a href="https://swiperjs.com/swiper-api" target="blank">
+                  <p className="px-3">SwiperJS</p>
+                </a>
+                <a
+                  href="https://www.npmjs.com/package/crypto-js"
+                  target="blank"
+                >
+                  <p className="px-3">Crypto-Js</p>
+                </a>
+                <a
+                  href="https://www.npmjs.com/package/jwt-decode"
+                  target="blank"
+                >
+                  <p className="px-3">JWT-Decode</p>
+                </a>
+                <a href="https://ngrx.io/guide/store" target="blank">
+                  <p className="px-3">Ngrx-Store</p>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </section>
   );
