ff00f4f5feebbdf286523c93cd4648e7c0370aab
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2023-12-31 12:15:31 -0400

    fix: CSS para ser um classe generica para <A>

--- app/src/components/infoProjetos/info_projeto.tsx
+++ app/src/components/infoProjetos/info_projeto.tsx
@@ -1,5 +1,6 @@
 "use client";
 import "./info_projeto.css";
+import "../../../styles/tagAEffect.css";
 import { useEffect, useState } from "react";
 
 export default function InfoProjetos() {
@@ -8,28 +9,56 @@ export default function InfoProjetos() {
   const toogleModalAberto = () => {
     setModalFechado(!modalFechado);
   };
+
   useEffect(() => {
     const fecharModalAoClicarFora = (event: MouseEvent) => {
-      const modal = document.getElementById('modal');
+      const modal = document.getElementById("modal");
 
-      if (!modalFechado && event.target instanceof Element && !modal?.contains(event.target as Node)) {
+      if (
+        !modalFechado &&
+        event.target instanceof Element &&
+        !modal?.contains(event.target as Node)
+      ) {
         setModalFechado(true);
       }
     };
 
-    document.addEventListener('click', fecharModalAoClicarFora);
+    document.addEventListener("click", fecharModalAoClicarFora);
 
     return () => {
-      document.removeEventListener('click', fecharModalAoClicarFora);
+      document.removeEventListener("click", fecharModalAoClicarFora);
     };
-
   }, [modalFechado]);
+const projetos = {[
+  "AreaInsted"]
+}
+  const areaInstedInfo = {
+    title: "Área Insted",
+    framework: {
+      title: "Framework",
+      items: [{ id: 1, name: "Angular 15.2", link: "https://angular.io/" }],
+    },
+    libraries: {
+      title: "Bibliotecas",
+      items: [{ 
+        [
+        { name: "Angular Material", link: "https://v15.material.angular.io/" },
+        { name: "SwiperJS", link: "https://swiperjs.com/swiper-api" },
+        { name: "Crypto-Js", link: "https://www.npmjs.com/package/crypto-js" },
+        {
+          name: "JWT-Decode",
+          link: "https://www.npmjs.com/package/jwt-decode",
+        },
+        { name: "Ngrx-Store", link: "https://ngrx.io/guide/store" },
+    ]}],
+    },
+  };
 
   return (
     <section className="relative infoSection">
       <button
         onClick={toogleModalAberto}
-        className=" w-10 h-10 bg-white dark:bg-dark border-2 dark:border-white border-dark rounded-full flex items-center p-1"
+        className="w-10 h-10 bg-white dark:bg-dark border-2 dark:border-white border-dark rounded-full flex items-center p-1"
       >
         <span className="material-symbols-rounded text-dark dark:text-white text-center m-auto">
           info
@@ -38,74 +67,60 @@ export default function InfoProjetos() {
       {modalFechado ? null : (
         <div
           id="modal"
-          className={`${modalFechado
-            ? "opacity-0 h-0 w-0 transition-none"
-            : "opacity-95 min-w-[200px] min-h-fit transition-opacity duration-150 linear"
-            } modal absolute p-1 bg-white dark:bg-dark border-2 border-dark dark:border-white white top-0 right-10 z-40 rounded-3xl m-2 shadow-md shadow-dark`}
+          className={`${
+            modalFechado
+              ? "opacity-0 h-0 w-0 transition-none"
+              : "opacity-95 min-w-[200px] min-h-fit transition-opacity duration-150 linear"
+          } modal absolute p-1 bg-white dark:bg-dark border-2 border-dark dark:border-white white top-0 right-10 z-40 rounded-3xl m-2 shadow-md shadow-dark`}
         >
-          <div
-            className=""
-          >
+          <div>
             <div className="">
               <h3
-                className={`'
-         ${modalFechado ? " opacity-0 transition-none" : "opacity-1 "}
-         text-dark dark:text-white text-center text-2xl p-2 
-         '
-         `}
+                className={`${
+                  modalFechado ? "opacity-0 transition-none" : "opacity-1"
+                } text-dark dark:text-white text-center text-2xl p-2`}
               >
-                Área Insted
+                {areaInstedInfo.title}
               </h3>
             </div>
             <nav
               id="navModal"
-              className={`' nav
-         ${modalFechado ? " opacity-0" : "opacity-1 "} 
-         '`}
+              className={`nav ${modalFechado ? "opacity-0" : "opacity-1"}`}
             >
-              <ul className={`' ${modalFechado ? " opacity-0" : "opacity-1"} ' dark:text-white  `}>
+              <ul
+                className={`${
+                  modalFechado ? "opacity-0" : "opacity-1"
+                } dark:text-white`}
+              >
                 <li>
-                  <h4 className="text-xl p-1">Framework</h4>
-                  <li>
-                    <a>
-                      <p className="px-3">Angular 15.2</p>
-                    </a>
-                  </li>
+                  <h4 className="text-xl p-1">
+                    {areaInstedInfo.framework.title}
+                  </h4>
+                  {areaInstedInfo.framework.items.map((item, index) => (
+                    <li key={index}>
+                      <a className="linkSeta" href={item.link}>
+                        <p className="px-3">{item.name}</p>
+                      </a>
+                    </li>
+                  ))}
                 </li>
                 <li>
-                  <li>
-                    <h4 className="text-xl p-1">Bibliotecas</h4>
-                  </li>
-                  <a
-                    className=""
-                    href="https://v15.material.angular.io/"
-                    target="blank"
-                  >
-                    <p className="px-3">Angular Material</p>
-                  </a>
-                  <a href="https://swiperjs.com/swiper-api" target="blank">
-                    <p className="px-3">SwiperJS</p>
-                  </a>
-                  <a
-                    href="https://www.npmjs.com/package/crypto-js"
-                    target="blank"
-                  >
-                    <p className="px-3">Crypto-Js</p>
-                  </a>
-                  <a
-                    href="https://www.npmjs.com/package/jwt-decode"
-                    target="blank"
-                  >
-                    <p className="px-3">JWT-Decode</p>
-                  </a>
-                  <a href="https://ngrx.io/guide/store" target="blank">
-                    <p className="px-3">Ngrx-Store</p>
-                  </a>
+                  <h4 className="text-xl p-1">
+                    {areaInstedInfo.libraries.title}
+                  </h4>
+                  {areaInstedInfo.libraries.items.map((item, index) => (
+                    <li key={index}>
+                      <a className="linkSeta" href={item.link} target="_blank">
+                        <p className="px-3">{item.name}</p>
+                      </a>
+                    </li>
+                  ))}
                 </li>
               </ul>
             </nav>
           </div>
-        </div>)}
+        </div>
+      )}
     </section>
   );
 }
