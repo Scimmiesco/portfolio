569157be1db53124bb6ad7e61dda57007e6c0f57
Scimmiesco <146399100+Scimmiesco@users.noreply.github.com>
2025-05-03 19:23:06 -0400

    Ajustes de estilização

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cookies } from "next/headers";
-import {AlumniSans, FiraCode} from "@/app/ui/fonts";
+import { AlumniSans, FiraCode } from "@/app/ui/fonts";
 import { Children, cloneElement } from "react";
 
 export const metadata: Metadata = {
-  title: "Pedro Almeida",
-  description: "Meu portfólio",
+    title: "Pedro Almeida",
+    description: "Meu portfólio",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+export default async function RootLayout({
+                                             children,
+                                         }: {
+    children: React.ReactNode;
 }) {
-  const temaCookie = cookies().get("tema")?.value || "light";
+    const cookiesStore = await cookies();
+    const temaCookie = cookiesStore.get("tema")?.value || "light";
 
-  return (
-    <html lang="pt-br">
-      <body
-        className={`${AlumniSans.variable} ${FiraCode.variable} antialiased w-full flex flex-col justify-start min-h-screen bg-white dark:bg-dark`}
-      >
+    return (
+        <html lang="pt-br" className={temaCookie}>
+        <body
+            className={`${AlumniSans.variable} ${FiraCode.variable} antialiased w-full flex flex-col justify-start min-h-screen bg-white dark:bg-dark`}
+        >
         {Children.map(children, (child) =>
-          cloneElement(child as React.ReactElement, { temaCookie })
+            cloneElement(child as React.ReactElement, { temaCookie })
         )}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
